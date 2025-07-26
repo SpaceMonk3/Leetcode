b18449adbd7a46a2d7c5b15b4f5330a6520f8a8c
Dheer Tammina <dtammina@gmu.edu>
2025-07-26 14:06:51 +0530

    updated encode/decode with better runtime and more notes

--- Arrays and Hashing/EncodeDecodeStrings/EncodeDecodeStrings.py
+++ Arrays and Hashing/EncodeDecodeStrings/EncodeDecodeStrings.py
@@ -1,40 +1,55 @@
 '''
 brute force:
-- encode -loop thru & choose a non ascii/ utf-8 character as an identifier and add it at the end of each string
-- !4neet!4code!3you!2we
-- 4neet44ode
-- decode -loop thru & remove strings before the non ascii character identifier and add them all to a single string and return
-- 
+- encode - loop thru each string and count length AND choose a character as an identifier and add both at the start of each string (length first then identifier)
+- 4!neet4!code3!you2!we - example
+- decode - loop thru string and get words based off the length and identifier & add them to a list and return
 
+-Notes of why other approaches didn't work:
+1) Just an identifier is not enough to encode/decode the string as there could be similar characters in the strings. Ex: ["name","na!e"] -> !name!na!e -> which would result in "na" instead of "na!e"
+2) Length of string is not enough to encode/decode the string as there could be lengths w double digits. Ex: is the length in "10namesnames" 1 or 10? How would we know? We add an identifier AFTER the length to solve this.
+3) Adding identifier before length (!4name) will not work as this brings us back to problem 2.
+
+Big O: O(n) for both encode and decode as long as string aren't being concatenated. (They are immutable in python & Java)
 '''
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        enc = ""
+        # enc = ""
+        # for s in strs:
+        #     enc += str(len(s)) + "!" + s  
+        #     print(enc) # testing
+        # return enc
+        '''Strings are immutable O(n^2) so we need to use a list to store the encoded strings for O(n)'''
+        enc = []
         for s in strs:
-            enc += str(len(s)) + "!" + s  
-            print(enc) # testing
-        
-        return enc
+            enc += str(len(s)) + "!" + s
+        return "".join(enc)
     
     def decode(self, s: str) -> List[str]:
         dec = []
-        length = ""
+        length = []
         c = 0
         while c < len(s):
-            string = ""
+            # string = ""
             if s[c] != '!':
-                length += s[c]
+                '''Strings are immutable O(n^2) so we need to use a list to store the encoded strings for O(n)'''
+                # length += s[c]
+                # c += 1
+                length.append(s[c])
                 c += 1
             else:
-                size = int(length)
-                c += 1
-                while(size != 0):
-                    string += s[c]    
-                    c += 1
-                    size -= 1
+                size = int("".join(length))
+                '''Strings are immutable O(n^2) so using a direct .append approach for O(n)'''
+                # c += 1
+                # while(size != 0):
+                #     string += s[c]    
+                #     c += 1
+                #     size -= 1
                 
-                length = ""
-                dec.append(string)
+                dec.append(s[c+1 : c+1+size])
+                c = c + 1 + size
+                length = []
             
         return dec
+    
+'''Code needs to be written more concisely'''
